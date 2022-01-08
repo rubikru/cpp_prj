0c84b791413bffec41638938c2c60dafd314abe2
Rustam Bikmukhametov <rbikmukhametov@diasoft.ru>
2022-01-08 07:46:28 +0300

    Уюраны лищние файлы

--- push.sh
+++ push.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-git push 'https://rubikru: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@github.com/rubikru/cpp_prj' --all
-
-#'https://rfb74@ya.ru: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@github.com/rubik/
-
-#git push 'https://rfb74@ya.ru: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@github.com/rubikru/cpp_prj' --all
+git push 'https://rubikru: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@github.com/rubikru/cpp_prj' --all
 
 
